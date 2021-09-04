25fbb5b6a72f8793da976aa048c513b4f6c20f2d
bikashchaurasia <BIKASH867492@GMAIL.COM>
2021-09-04 09:08:26 +0530

    Initial commit

---
+++ tracking.py
@@ -0,0 +1,11 @@
+import phonenumbers
+from phonenumbers import geocoder
+phone_numbers1 = phonenumbers.parse("+918809487041")
+phone_numbers2 = phonenumbers.parse("+4930901820")
+phone_numbers3 = phonenumbers.parse("+13478439904")
+print(geocoder.description_for_number(phone_numbers1 , "en"))
+print(geocoder.description_for_number(phone_numbers2 , "en"))
+print(geocoder.description_for_number(phone_numbers3 , "en"))
+
+
+
